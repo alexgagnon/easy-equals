cb82b5eabc0f9bab0351268fc465a4dc3b82bca2
alexgagnon <asgagnon@hotmail.com>
2016-06-08 20:08:12 -0400

    Testing on large object

--- easy-equals.js
+++ easy-equals.js
@@ -3,15 +3,19 @@ function checkEquals(o1, o2, proto) {
   var t2 = typeof o2;
   if (t1 !== t2) return false; //they aren't the same type, can't be equal
 
-  //null's typeof is 'object', even though we can't do anything with it. Need to capture all primitives, undefined, and nulls here
+  //null's typeof is 'object', even though we can't do anything with it. Need to capture all primitives, undefined, and nulls here (not functions though)
   if ((t1 !== 'object' && t1 !== 'function') || o1 === null || o2 === null) {
     return o1 === o2;
   }
 
   //if it is a function passed in and it isn't the same ref, we can only go off it's toString, which will match both anonymous and named functions
   if (t1 === 'function') {
-    console.log('hello', o1.toString(), o2.toString());
     return o1.toString() === o2.toString();
+  }
+
+  //compare dates using getTime()
+  if (o1 instanceof Date) {
+    return (o2 instanceof Date) ? o1.getTime() === o2.getTime() : false;
   }
 
   //if it is an array passed in
@@ -22,16 +26,17 @@ function checkEquals(o1, o2, proto) {
   //if we don't have the same number of keys, must be false
   if (Object.keys(o1).length !== Object.keys(o2).length) return false;
 
-  //loop through the keys, and recursively see if they're the same
-  for (var propName in o1) {
-    if (o1.hasOwnProperty(propName)) {
-      if (!checkEquals(o1[propName], o2[propName])) {
-        return false;
-      }
-    }
+  //loop through own properties, and recursively see if they're the same. To limit comparisons with hasOwnProperty, use try/catch to break out of the forEach
+  try {
+    Object.keys(o1).forEach(function(propName) {
+      if (!checkEquals(o1[propName], o2[propName])) throw BreakException();
+    })
+  }
+  catch (e) {
+    return false;
   }
 
-  //all this objects' keys are correct, check the proto if desired
+  //all this objects' keys are correct, check the proto if desired. This speeds up looking through all props, as if the proto are the same ref, catches it in one check
   if (proto === undefined || proto === true) {
     if (!checkEquals(Object.getPrototypeOf(o1), Object.getPrototypeOf(o2), true)) {
       return false;
@@ -42,11 +47,11 @@ function checkEquals(o1, o2, proto) {
 }
 
 //makes it easier to check protos
-function checkProto(o1, o2, proto, arrOrder) {
+function checkProto(o1, o2, proto) {
   return checkEquals(Object.getPrototypeOf(o1), Object.getPrototypeOf(o2), proto)
 }
 
 module.exports = {
-  checkEquals: checkEquals,
-  checkProto: checkProto,
+  equals: checkEquals,
+  equalsProto: checkProto,
 }
